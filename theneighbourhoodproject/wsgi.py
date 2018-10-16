e4860bb40361958a24a0579672bbf8b5c5c9d7a6
melisamalala <melissamalala@gmail.com>
2018-10-16 15:21:46 +0300

    migration files

--- theneighbourhoodproject/wsgi.py
+++ theneighbourhoodproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for neighbourhoodproject project.
+WSGI config for theneighbourhoodproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neighbourhoodproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theneighbourhoodproject.settings")
 
 application = get_wsgi_application()
