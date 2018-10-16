139d57c6ea993eca4adbab5227d45e6f14a2c701
melisamalala <melissamalala@gmail.com>
2018-10-16 16:53:33 +0300

    deploy

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -17,3 +17,28 @@ $(document).ready(function(){
   }) // End of submit event
 
 }) // End of document ready function
+
+
+$('#myCarousel').carousel({
+            interval: false
+        });
+
+        //scroll slides on swipe for touch enabled devices
+
+        $("#myCarousel").on("touchstart", function(event){
+
+            var yClick = event.originalEvent.touches[0].pageY;
+            $(this).one("touchmove", function(event){
+
+                var yMove = event.originalEvent.touches[0].pageY;
+                if( Math.floor(yClick - yMove) > 1 ){
+                    $(".carousel").carousel('next');
+                }
+                else if( Math.floor(yClick - yMove) < -1 ){
+                    $(".carousel").carousel('prev');
+                }
+            });
+            $(".carousel").on("touchend", function(){
+                $(this).off("touchmove");
+            });
+        });
