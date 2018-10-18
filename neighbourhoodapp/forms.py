f3807fbd47dbfa0391a5865ec35814e3aba01451
melisamalala <melissamalala@gmail.com>
2018-10-18 14:15:22 +0300

    Can create new business

--- neighbourhoodapp/forms.py
+++ neighbourhoodapp/forms.py
@@ -32,8 +32,12 @@ class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
 
-
 class CreateNeighbourhoodForm(forms.ModelForm):
     class Meta:
         model = Neighbourhood
+        exclude = ['user',]
+
+class NewBusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
         exclude = ['user',]
--- neighbourhoodapp/views.py
+++ neighbourhoodapp/views.py
@@ -6,11 +6,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .forms import *
 from .email import send_welcome_email
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from .serializers import *
-from rest_framework import status
-from .permissions import IsAdminOrReadOnly
 
 
 @login_required(login_url='/accounts/login/')
@@ -92,6 +87,36 @@ def new_image(request):
         form = NewImageForm()
     return render(request, 'registration/new_image.html', {"form": form})
 
+@login_required(login_url='/accounts/login/')
+def new_business(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewBusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.user = current_user
+            business.save()
+        return redirect('homePage')
+
+    else:
+        form = NewBusinessForm()
+    return render(request, 'registration/new_business.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def new_business(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewBusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.user = current_user
+            business.save()
+        return redirect('homePage')
+
+    else:
+        form = NewBusinessForm()
+    return render(request, 'registration/new_business.html', {"form": form})
+
 
 @login_required(login_url='/accounts/login/')
 def new_project(request):
@@ -123,6 +148,9 @@ def new_neighbourhood(request):
     else:
         form = CreateNeighbourhoodForm()
     return render(request, 'registration/new_neighbourhood.html', {"form": form})
+
+
+
 
 
 # Viewing a single picture
@@ -240,102 +268,20 @@ def newsletter(request):
     data= {'success': 'You have been successfully added to the newsletter mailing list'}
     return JsonResponse(data)
 
-# Project Serializer
+@login_required(login_url='/accounts/login/')
+def join_neighbourhood(request, id):
+    '''
+    This view function will implement adding
+    '''
 
-class ProjectList(APIView):
-    def get(self, request, format = None):
-        all_projects = Project.objects.all()
-        serializers = ProjectSerializer(all_projects, many = True)
-        return Response(serializers.data)
-
-
-    def post(self, request, format=None):
-        serializers = ProjectSerializer(data=request.data)
-        permission_classes = (IsAdminOrReadOnly,)
-
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status= status.HTTP_201_CREATED)
-
-        return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
+    neighbourhood = Neighbourhood.objects.get(pk=id)
+    if Join.objects.filter(user_id=request.user).exists():
 
 
-class ProjectDescription(APIView):
-    permission_classes = (IsAdminOrReadOnly,)
-    def get_project(self, pk):
-        try:
-            return Project.objects.get(pk=pk)
-        except Project.DoesNotExist:
-            return Http404
+        Join.objects.filter(user_id=request.user).update(neighbourhood_id=neighbourhood)
+    else:
 
-    def get(self, request, pk, format=None):
-        project = self.get_project(pk)
-        serializers = ProjectSerializer(project)
-        return Response(serializers.data)
+        Join(user_id=request.user, neighbourhood_id=neighbourhood).save()
 
 
-    def put(self, request, pk, format = None):
-        project = self.get_project(pk)
-        serializers = ProjectSerializer(project, request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
-
-        else:
-            return Response(serializers.errors,
-                            status = status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        project = self.get_project(pk)
-        project.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-# Profile Serializer
-
-class ProfileList(APIView):
-    def get(self, request, format = None):
-        all_profiles = Profile.objects.all()
-        serializers = ProfileSerializer(all_profiles, many = True)
-        return Response(serializers.data)
-
-
-    def post(self, request, format=None):
-        serializers = ProfileSerializer(data=request.data)
-        permission_classes = (IsAdminOrReadOnly,)
-
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status= status.HTTP_201_CREATED)
-
-        return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
-
-
-class ProfileDescription(APIView):
-    permission_classes = (IsAdminOrReadOnly,)
-    def get_profile(self, pk):
-        try:
-            return Profile.objects.get(pk=pk)
-        except Profile.DoesNotExist:
-            return Http404
-
-    def get(self, request, pk, format=None):
-        profile = self.get_profile(pk)
-        serializers = ProfileSerializer(profile)
-        return Response(serializers.data)
-
-
-    def put(self, request, pk, format = None):
-        profile = self.get_profile(pk)
-        serializers = ProfileSerializer(profile, request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
-
-        else:
-            return Response(serializers.errors,
-                            status = status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        profile = self.get_profile(pk)
-        profile.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    return render(request, 'neighbourhoods.html', )
