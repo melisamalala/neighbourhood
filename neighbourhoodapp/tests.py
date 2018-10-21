2b415e9ca29e145da355071246d672f28e2e66af
melisamalala <melissamalala@gmail.com>
2018-10-21 12:07:44 +0300

    Tests complete

--- neighbourhoodapp/tests.py
+++ neighbourhoodapp/tests.py
@@ -1,14 +1,101 @@
-# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
-from django.test import TestCase
-
-# Create your tests here.
-from .models import Location, tags, Image, Review, User, Profile, Neighbourhood
+from .models import *
 from django.core.files.uploadedfile import SimpleUploadedFile
+
+class NeighbourhoodTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.nairobi = Location.objects.create(name="nairobi")
+
+        self.test_neighbourhood = Neighbourhood.objects.create(neighbourhood_name='imagesef',
+                                population='1000',
+                                neighbourhood_location=self.nairobi,
+                                )
+
+        self.test_neighbourhood.save()
+
+    def test_save_method(self):
+        self.test_neighbourhood.save()
+        test_neighbourhoods = Neighbourhood.objects.all()
+        self.assertTrue(len(test_neighbourhoods) > 0)
+
+    # Testing save method
+    def test_save_neighbourhood(self):
+        self.assertEqual(len(Neighbourhood.objects.all()), 1)
+
+    # Tear down method
+    def tearDown(self):
+        Neighbourhood.objects.all().delete()
+
+    def test_delete_neighbourhood(self):
+        Neighbourhood.delete_image_by_id(self.test_neighbourhood.id)
+        self.assertEqual(len(Neighbourhood.objects.all()), 0)
+
+
+class ProjectTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.nairobi = Neighbourhood.objects.create(name="nairobi")
+
+        self.test_project = Project.objects.create(image='imagesef',
+                                                   name='cat',
+                                                   description='This is a description',
+                                                   neighbourhood_id=self.nairobi,
+                                                   )
+
+        self.test_project.save()
+
+    def test_save_method(self):
+        self.test_project.save()
+        test_projects = Project.objects.all()
+        self.assertTrue(len(test_projects) > 0)
+
+    # Testing save method
+    def test_save_project(self):
+        self.assertEqual(len(Project.objects.all()), 1)
+
+    # Tear down method
+    def tearDown(self):
+        Project.objects.all().delete()
+
+    def test_delete_project(self):
+        Project.delete_image_by_id(self.test_project.id)
+        self.assertEqual(len(Project.objects.all()), 0)
+
+
+class BusinessTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.nairobi = Neighbourhood.objects.create(name="nairobi")
+
+        self.test_business = Business.objects.create(image='imagesef',
+                                                     business_name='cat',
+                                                     business_email_address='business@gmail.com',
+                                                     neighbourhood_id=self.nairobi,
+                                                     )
+
+        self.test_business.save()
+
+    def test_save_method(self):
+        self.test_business.save()
+        test_businesses = Business.objects.all()
+        self.assertTrue(len(test_businesses) > 0)
+
+    # Testing save method
+    def test_save_business(self):
+        self.assertEqual(len(Business.objects.all()), 1)
+
+    # Tear down method
+    def tearDown(self):
+        Business.objects.all().delete()
+
+    def test_delete_image(self):
+        Business.delete_business_by_id(self.test_business.id)
+        self.assertEqual(len(Business.objects.all()), 0)
 
 
 class tagsTestClass(TestCase):
