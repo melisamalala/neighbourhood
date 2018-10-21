ea78483c30bc31173d1cdfa87cf26c95dd49e04e
melisamalala <melissamalala@gmail.com>
2018-10-21 11:41:46 +0300

    deploy

--- neighbourhoodapp/tests.py
+++ neighbourhoodapp/tests.py
@@ -75,42 +75,6 @@ class LocationTestClass(TestCase):
 
 
 
-class ImageTestClass(TestCase):
-    # Set up method
-    def setUp(self):
-
-
-        self.nairobi = Location.objects.create(name="nairobi")
-        self.funny= tags.objects.create(name='funny')
-
-
-        self.test_image = Image.objects.create(image='imagesef',
-                                name='cat',
-                                description='This is a description',
-                                location=self.nairobi,
-                                )
-
-        self.test_image.save()
-
-    def test_save_method(self):
-        self.test_image.save()
-        test_images = Image.objects.all()
-        self.assertTrue(len(test_images) > 0)
-
-    # Testing save method
-    def test_save_image(self):
-        self.assertEqual(len(Image.objects.all()), 1)
-
-    # Tear down method
-    def tearDown(self):
-        Image.objects.all().delete()
-
-    def test_delete_image(self):
-        Image.delete_image_by_id(self.test_image.id)
-        self.assertEqual(len(Image.objects.all()), 0)
-
-
-
 class Review(TestCase):
 
     def setUp(self):
